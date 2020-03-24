1296e41255ba7133aabf957aeb25be50442a1d39
MSI\mitchm <mmaynar1@gmail.com>
2020-03-23 21:42:01 -0500

    Generate test full of random major third intervals

--- ear-trainer/main.js
+++ ear-trainer/main.js
@@ -1,4 +1,59 @@
+let questions = [];
+let answers = [];
+let questionIndex = 0;
+
 let playInterval = function( id )
 {
     document.getElementById(id).play();
+}
+
+let generateTest = function()
+{
+    let questionCount = 20;
+    questions = [];
+    answers = [];
+    questionIndex = 0;
+
+    for( let question = 0; question < questionCount; question++ )
+    {
+        let interval = getRandomInterval();
+        questions[question] = interval + getRandomIndex();
+        answers[question] = interval;
+    }
+
+    playInterval( questions[questionIndex] );
+}
+
+let replayInterval = function()
+{
+    playInterval( questions[questionIndex] );
+}
+
+let getRandomInterval = function()
+{
+    let interval = getRandomInteger( 0, 4 );
+    let intervalName = "third";
+    switch( interval )
+    {
+      case 0:
+        intervalName = "third"
+        break;
+/*      case 1:
+        intervalName = "fourth"
+        break;*/
+      default:
+        // code block
+    }
+
+    return intervalName
+}
+
+let getRandomIndex = function()
+{
+	return getRandomInteger( 0, 7 );
+}
+
+let getRandomInteger = function( min, max )
+{
+	return Math.floor( Math.random() * ( max - min ) ) + min;
 }
